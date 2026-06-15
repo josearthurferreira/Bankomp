62cf57036df32610c64450838c7bd0cf47a2178b
José Arthur Ferreira Cardoso <josearthurferreiracardoso@gmail.com>
2026-06-14 23:00:32 -0300

    chore: update windows bat script with wildcard matching for dynamic compilation

--- run.bat
+++ run.bat
@@ -1,7 +1,12 @@
 @echo off
 set BANKOMP_KEY=IME_SE9_SECRET_KEY_2026
-g++ -std=c++17 main.cpp Control/Bank.cpp Interface/InterfaceConsole.cpp Model/Account.cpp Model/Client.cpp Model/CurrentAccount.cpp Model/SavingAccount.cpp Storage/Cryptography.cpp Storage/DataManager.cpp -o bankomp.exe
+g++ -std=c++17 main.cpp Control/*.cpp Interface/*.cpp Model/*.cpp Storage/*.cpp -o bankomp.exe
 if %errorlevel% equ 0 (
+    cls
     bankomp.exe
+) else (
+    echo.
+    echo [ERRO] Falha na compilacao do projeto.
+    echo.
 )
 pause
